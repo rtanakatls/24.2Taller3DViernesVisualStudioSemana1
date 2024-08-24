fd654ff9c2ce4fa51c52e082bd9422ef4ad8e2cb
Ricardo Tanaka Terukina <ricardotanaka-externo@tgacompany.com>
2024-08-23 19:25:39 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24._2Taller3DSemana1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
